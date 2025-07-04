4404847df6a3422568b41cf5af4067528997184f
schelskedevco <joe@schelske.dev>
2025-07-04 16:20:02 -0500

    basic 3col layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full bg-white">
       <body
-        className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
+        className={`${inter.variable} ${spaceGrotesk.variable} h-full antialiased`}
       >
         {children}
       </body>
