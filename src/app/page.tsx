a2b716d3dc4950f6e66338fa09daf04920d77dc3
schelskedevco <joe@schelske.dev>
2025-07-07 22:37:00 -0500

    initial bento grid section for link sharing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Footer from "./components/footer";
 import CTASection from "./components/cta-section";
 import FAQSection from "./components/faq-section";
 import LogoCloud from "./components/logo-cloud";
+import LinkSharingFeature from "./components/link-sharing-feature";
 
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
       <LogoCloud />
+      <LinkSharingFeature />
       <FAQSection />
       <CTASection />
       <Footer />
