c55f7e0260c1392a5c46527ca8730bb3169724a6
schelskedevco <joe@schelske.dev>
2025-07-07 22:28:52 -0500

    initial logo cloud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import HeroSection from "./components/hero-section";
 import Footer from "./components/footer";
 import CTASection from "./components/cta-section";
 import FAQSection from "./components/faq-section";
+import LogoCloud from "./components/logo-cloud";
 
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
+      <LogoCloud />
       <FAQSection />
       <CTASection />
       <Footer />
