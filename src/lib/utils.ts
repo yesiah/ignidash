afb6d113d4a94fb139b4bda75fcada9a6f3933df
schelskedevco <joe@schelske.dev>
2025-07-17 00:05:57 -0500

    format #

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,13 @@
-export function cn(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
 
-export const formatNumber = (num: number, fractionDigits: number = 2) => {
+export function formatNumber(num: number, fractionDigits: number = 2) {
   if (num >= 1000000000) return (num / 1000000000).toFixed(fractionDigits) + 'B';
   if (num >= 1000000) return (num / 1000000).toFixed(fractionDigits) + 'M';
   if (num >= 1000) return (num / 1000).toFixed(fractionDigits) + 'k';
   return num.toString();
-};
+}
