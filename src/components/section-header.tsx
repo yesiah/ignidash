1c15e794683f2620dcce6c3ef61fab5e9b0c4610
schelskedevco <joe@schelske.dev>
2025-07-17 13:00:45 -0500

    section header

--- src/components/section-header.tsx
+++ src/components/section-header.tsx
@@ -6,8 +6,8 @@ interface SectionHeaderProps {
 export function SectionHeader({ title, desc }: SectionHeaderProps) {
   return (
     <div className="ml-2">
-      <h4 className="text-base font-semibold">{title}</h4>
-      <p className="text-muted-foreground mt-2 text-sm">{desc}</p>
+      <h3 className="text-xl font-semibold tracking-tight">{title}</h3>
+      <p className="text-muted-foreground mt-2 text-base">{desc}</p>
     </div>
   );
 }
