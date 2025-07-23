59d2a7531a796a905d2876ff2b3f364bb4a1b836
schelskedevco <joe@schelske.dev>
2025-07-23 11:14:50 -0500

    rename

--- src/app/dashboard/quick-plan/components/inputs/numbers-column-sections.tsx
+++ src/app/dashboard/quick-plan/components/inputs/numbers-column-sections.tsx
@@ -1,6 +1,6 @@
 import BasicsSection from './sections/basics/section';
 import GoalSection from './sections/retirement-goal/section';
-import FineTuneSection from './sections/fine-tune-section';
+import FineTuneSection from './sections/fine-tune/section';
 import FIREPathsSection from './sections/fire-paths-section';
 
 export default function NumbersColumnSections() {
