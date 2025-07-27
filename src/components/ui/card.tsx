83b540588fa71135a5ba0935178e556b3368a168
schelskedevco <joe@schelske.dev>
2025-07-27 13:45:21 -0500

    rounded lg

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 export default function Card({ children, className }: CardProps) {
   return (
-    <div className={cn('bg-emphasized-background border-border my-4 overflow-hidden rounded-xl border shadow-md', className)}>
+    <div className={cn('bg-emphasized-background border-border my-4 overflow-hidden rounded-lg border shadow-md', className)}>
       <div className="px-4 py-5 sm:p-6">{children}</div>
     </div>
   );
