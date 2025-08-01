ff4eca322e16d74117c2c6dcc475ecd7e34b7d01
schelskedevco <joe@schelske.dev>
2025-07-31 23:41:39 -0500

    wire up 2 results

--- src/app/dashboard/quick-plan/components/outputs/results-sections.tsx
+++ src/app/dashboard/quick-plan/components/outputs/results-sections.tsx
@@ -1,33 +1,47 @@
 'use client';
 
-import { useIsCalculationReady } from '@/lib/stores/quick-plan-store';
+import { useIsCalculationReady, useMarketAssumptionsData } from '@/lib/stores/quick-plan-store';
 import Card from '@/components/ui/card';
 import SectionHeader from '@/components/ui/section-header';
 import SectionContainer from '@/components/ui/section-container';
 
 import ResultsMetrics from './results-metrics';
-// import ResultsChart from './charts/results-chart';
+import ResultsChart from './charts/results-chart';
 import StochasticResultsChart from './charts/stochastic-results-chart';
 
 export default function ResultsSections() {
   const isCalculationReady = useIsCalculationReady();
-  if (isCalculationReady) {
+  const marketAssumptions = useMarketAssumptionsData();
+
+  if (!isCalculationReady) {
     return (
-      <SectionContainer showBottomBorder>
-        <SectionHeader title="Overview" desc="Timeline, milestones, and portfolio projections in one view." />
-        <ResultsMetrics />
-        <Card>
-          <h4 className="text-foreground mb-4 text-center text-lg font-semibold sm:text-left">Portfolio Projection</h4>
-          {/* <ResultsChart /> */}
-          <StochasticResultsChart />
-        </Card>
-      </SectionContainer>
+      <div className="text-muted-foreground text-center">
+        <p>Results content will be displayed here</p>
+      </div>
     );
   }
 
+  let resultsChart;
+  switch (marketAssumptions.simulationMode) {
+    case 'fixedReturns':
+      resultsChart = <ResultsChart />;
+      break;
+    case 'monteCarlo':
+      resultsChart = <StochasticResultsChart />;
+      break;
+    case 'historicalBacktest':
+      resultsChart = <StochasticResultsChart />;
+      break;
+  }
+
   return (
-    <div className="text-muted-foreground text-center">
-      <p>Results content will be displayed here</p>
-    </div>
+    <SectionContainer showBottomBorder>
+      <SectionHeader title="Overview" desc="Timeline, milestones, and portfolio projections in one view." />
+      <ResultsMetrics />
+      <Card>
+        <h4 className="text-foreground mb-4 text-center text-lg font-semibold sm:text-left">Portfolio Projection</h4>
+        {resultsChart}
+      </Card>
+    </SectionContainer>
   );
 }
