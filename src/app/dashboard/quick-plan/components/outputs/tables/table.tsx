abee3af689148e2b229acb0ec7f00170b2be93c6
schelskedevco <joe@schelske.dev>
2025-08-02 15:58:16 -0500

    table

--- src/app/dashboard/quick-plan/components/outputs/tables/table.tsx
+++ src/app/dashboard/quick-plan/components/outputs/tables/table.tsx
@@ -12,11 +12,11 @@ export default function Table() {
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="mt-8 flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <div className="inline-block min-w-full py-2 align-middle">
             <table className="relative min-w-full divide-y divide-gray-300">
               <thead>
                 <tr>
-                  <th scope="col" className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                  <th scope="col" className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">
                     Name
                   </th>
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
@@ -28,19 +28,19 @@ export default function Table() {
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                     Role
                   </th>
-                  <th scope="col" className="py-3.5 pr-4 pl-3 sm:pr-0">
+                  <th scope="col" className="py-3.5 pr-4 pl-3 sm:pr-6 lg:pr-8">
                     <span className="sr-only">Edit</span>
                   </th>
                 </tr>
               </thead>
-              <tbody className="divide-y divide-gray-200">
+              <tbody className="divide-y divide-gray-200 bg-white">
                 {people.map((person) => (
                   <tr key={person.email}>
-                    <td className="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0">{person.name}</td>
+                    <td className="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-6 lg:pl-8">{person.name}</td>
                     <td className="px-3 py-4 text-sm whitespace-nowrap text-gray-500">{person.title}</td>
                     <td className="px-3 py-4 text-sm whitespace-nowrap text-gray-500">{person.email}</td>
                     <td className="px-3 py-4 text-sm whitespace-nowrap text-gray-500">{person.role}</td>
-                    <td className="py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-0">
+                    <td className="py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-6 lg:pr-8">
                       <a href="#" className="text-indigo-600 hover:text-indigo-900">
                         Edit<span className="sr-only">, {person.name}</span>
                       </a>
