4812bbcc688600e489dc0759ccdb2fc32643db49
schelskedevco <joe@schelske.dev>
2025-08-03 16:56:58 -0500

    consts for metrics

--- src/app/dashboard/quick-plan/components/outputs/fixed-returns-metrics.tsx
+++ src/app/dashboard/quick-plan/components/outputs/fixed-returns-metrics.tsx
@@ -8,14 +8,21 @@ interface ResultsMetricsProps {
 }
 
 export default function ResultsMetrics({ fireAnalysis }: ResultsMetricsProps) {
+  const progressToFIRE = `${formatNumber(fireAnalysis.progressToFIRE * 100, 0)}%`;
+  const fireAge = fireAnalysis.fireAge !== null ? `${formatNumber(fireAnalysis.fireAge, 0)}` : '∞';
+  const yearsToFIRE = fireAnalysis.yearsToFIRE !== null ? `${formatNumber(fireAnalysis.yearsToFIRE, 0)}` : '∞';
+  const requiredPortfolio = `$${formatNumber(fireAnalysis.requiredPortfolio, 2)}`;
+  const finalPortfolio = `$${formatNumber(fireAnalysis.finalPortfolio, 2)}`;
+  const returnOnInvestment = `${((fireAnalysis.performance ?? 0) * 100).toFixed(1)}%`;
+
   return (
     <dl className="mt-4 mb-8 grid grid-cols-2 gap-2 2xl:grid-cols-3">
-      <MetricsCard name="Progress to FIRE" stat={`${formatNumber(fireAnalysis.progressToFIRE * 100, 0)}%`} />
-      <MetricsCard name="FIRE Age" stat={fireAnalysis.fireAge !== null ? `${formatNumber(fireAnalysis.fireAge, 0)}` : '∞'} />
-      <MetricsCard name="Years to FIRE" stat={fireAnalysis.yearsToFIRE !== null ? `${formatNumber(fireAnalysis.yearsToFIRE, 0)}` : '∞'} />
-      <MetricsCard name="Required Portfolio" stat={`$${formatNumber(fireAnalysis.requiredPortfolio, 2)}`} />
-      <MetricsCard name="Final Portfolio" stat={`$${formatNumber(fireAnalysis.finalPortfolio, 2)}`} />
-      <MetricsCard name="Return on Investment" stat={`${((fireAnalysis.performance ?? 0) * 100).toFixed(1)}%`} />
+      <MetricsCard name="Progress to FIRE" stat={progressToFIRE} />
+      <MetricsCard name="FIRE Age" stat={fireAge} />
+      <MetricsCard name="Years to FIRE" stat={yearsToFIRE} />
+      <MetricsCard name="Required Portfolio" stat={requiredPortfolio} />
+      <MetricsCard name="Final Portfolio" stat={finalPortfolio} />
+      <MetricsCard name="Return on Investment" stat={returnOnInvestment} />
     </dl>
   );
 }
