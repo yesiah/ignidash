b44d85256cefa5a6dafa3c2472c999021fa074d7
schelskedevco <joe@schelske.dev>
2025-08-07 11:33:31 -0500

    full length borders in col header

--- src/components/ui/column-header.tsx
+++ src/components/ui/column-header.tsx
@@ -8,8 +8,8 @@ interface ColumnHeaderProps {
 
 export default function ColumnHeader({ title, icon: Icon, iconButton }: ColumnHeaderProps) {
   return (
-    <div className="border-border mb-5 border-b pb-5">
-      <div className="flex items-center justify-between">
+    <div className="border-border -mx-4 mb-5 border-b pb-5 sm:-mx-6 lg:-mx-8">
+      <div className="mx-4 flex items-center justify-between sm:mx-6 lg:mx-8">
         <h2 className="flex items-center gap-2 text-2xl font-extrabold tracking-tight">
           <Icon className="text-primary h-8 w-8" aria-hidden="true" />
           {title}
