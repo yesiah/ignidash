5a7bef6d4f995dffb601ee8093a8860f208ac12f
schelskedevco <joe@schelske.dev>
2025-08-20 00:25:40 -0500

    border border

--- src/components/catalyst/divider.tsx
+++ src/components/catalyst/divider.tsx
@@ -2,15 +2,6 @@ import clsx from 'clsx';
 
 export function Divider({ soft = false, className, ...props }: { soft?: boolean } & React.ComponentPropsWithoutRef<'hr'>) {
   return (
-    <hr
-      role="presentation"
-      {...props}
-      className={clsx(
-        className,
-        'w-full border-t',
-        soft && 'border-stone-950/5 dark:border-white/5',
-        !soft && 'border-stone-950/10 dark:border-white/10'
-      )}
-    />
+    <hr role="presentation" {...props} className={clsx(className, 'w-full border-t', soft && 'border-border', !soft && 'border-border')} />
   );
 }
