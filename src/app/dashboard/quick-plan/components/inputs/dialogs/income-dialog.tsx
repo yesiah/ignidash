e69cbc243a7edc7f2210bfb1611a17ffaac927d8
schelskedevco <joe@schelske.dev>
2025-08-20 22:38:39 -0500

    add limit field

--- src/app/dashboard/quick-plan/components/inputs/dialogs/income-dialog.tsx
+++ src/app/dashboard/quick-plan/components/inputs/dialogs/income-dialog.tsx
@@ -3,7 +3,7 @@
 import { DialogTitle, DialogBody, DialogActions } from '@/components/catalyst/dialog';
 import NumberInput from '@/components/ui/number-input';
 import { Field, FieldGroup, Fieldset, Label /* Description */ } from '@/components/catalyst/fieldset';
-// import { Divider } from '@/components/catalyst/divider';
+import { Divider } from '@/components/catalyst/divider';
 import { Select } from '@/components/catalyst/select';
 import { Button } from '@/components/catalyst/button';
 import { Input } from '@/components/catalyst/input';
@@ -21,39 +21,62 @@ export default function IncomeDialog({ incomeDialogOpen, setIncomeDialogOpen }: 
         <form onSubmit={(e) => e.preventDefault()}>
           <Fieldset aria-label="Income details">
             <FieldGroup>
-              <Field>
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" name="name" placeholder="My Salary" autoComplete="off" inputMode="text" />
-              </Field>
-              <Field>
-                <Label htmlFor="amount">Amount</Label>
-                <NumberInput
-                  id="amount"
-                  value={85000}
-                  onBlur={(value) => {
-                    return { success: true };
-                  }}
-                  inputMode="decimal"
-                  placeholder="$85,000"
-                  prefix="$"
-                />
-              </Field>
-              <Field>
-                <Label htmlFor="growth-rate" className="flex w-full items-center justify-between">
-                  <span>Rate of Change</span>
-                  <span className="text-muted-foreground text-sm/6">{Number(3).toFixed(1)}% real</span>
-                </Label>
-                <NumberInput
-                  id="growth-rate"
-                  value={3}
-                  onBlur={(value) => {
-                    return { success: true };
-                  }}
-                  inputMode="decimal"
-                  placeholder="3%"
-                  suffix="%"
-                />
-              </Field>
+              <div className="grid grid-cols-2 gap-6">
+                <Field>
+                  <Label htmlFor="name">Name</Label>
+                  <Input id="name" name="name" placeholder="My Salary" autoComplete="off" inputMode="text" />
+                </Field>
+                <Field>
+                  <Label htmlFor="amount">Amount</Label>
+                  <NumberInput
+                    id="amount"
+                    value={85000}
+                    onBlur={(value) => {
+                      return { success: true };
+                    }}
+                    inputMode="decimal"
+                    placeholder="$85,000"
+                    prefix="$"
+                  />
+                </Field>
+              </div>
+              <Divider />
+              <div className="grid grid-cols-5 gap-6">
+                <div className="col-span-3">
+                  <Field>
+                    <Label htmlFor="growth-rate" className="flex w-full items-center justify-between">
+                      <span>Growth Rate</span>
+                      <span className="text-muted-foreground text-sm/6">{Number(3).toFixed(1)}% real</span>
+                    </Label>
+                    <NumberInput
+                      id="growth-rate"
+                      value={3}
+                      onBlur={(value) => {
+                        return { success: true };
+                      }}
+                      inputMode="decimal"
+                      placeholder="3%"
+                      suffix="%"
+                    />
+                  </Field>
+                </div>
+                <div className="col-span-2">
+                  <Field>
+                    <Label htmlFor="growth-limit">Limit</Label>
+                    <NumberInput
+                      id="growth-limit"
+                      value={null}
+                      onBlur={(value) => {
+                        return { success: true };
+                      }}
+                      inputMode="decimal"
+                      placeholder="$120,000"
+                      prefix="$"
+                    />
+                  </Field>
+                </div>
+              </div>
+              <Divider />
               <Field>
                 <Label htmlFor="frequency">Frequency</Label>
                 <Select id="frequency" name="frequency">
