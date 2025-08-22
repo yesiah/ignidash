d5f9d06f5d54b4e5481d55e945a25c6d092cab3c
schelskedevco <joe@schelske.dev>
2025-08-22 01:04:07 -0500

    income dialog

--- src/app/dashboard/quick-plan/components/inputs/dialogs/income-dialog.tsx
+++ src/app/dashboard/quick-plan/components/inputs/dialogs/income-dialog.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
 import { MinusIcon, PlusIcon, ArrowTrendingUpIcon } from '@heroicons/react/24/outline';
 import { /* CoinsIcon, */ CalendarIcon, BanknoteArrowUpIcon } from 'lucide-react';
@@ -15,6 +16,12 @@ import { Combobox, ComboboxLabel, ComboboxOption } from '@/components/catalyst/c
 import { Select } from '@/components/catalyst/select';
 import { Button } from '@/components/catalyst/button';
 import { Input } from '@/components/catalyst/input';
+
+interface ActiveDisclosurePanelData {
+  open: boolean;
+  close: () => void;
+  key: string;
+}
 
 interface IncomeDialogProps {
   incomeDialogOpen: boolean;
@@ -87,6 +94,21 @@ export default function IncomeDialog({ incomeDialogOpen, setIncomeDialogOpen }: 
   // const lifeExpectancy = useLifeExpectancy()!;
   // const ages = Array.from({ length: lifeExpectancy - currentAge + 1 }, (_, i) => currentAge + i);
 
+  const [activeDisclosurePanel, setActiveDisclosurePanel] = useState<ActiveDisclosurePanelData | null>(null);
+
+  function togglePanels(newPanel: ActiveDisclosurePanelData) {
+    if (activeDisclosurePanel) {
+      if (activeDisclosurePanel.key !== newPanel.key && activeDisclosurePanel.open) {
+        activeDisclosurePanel.close();
+      }
+    }
+
+    setActiveDisclosurePanel({
+      ...newPanel,
+      open: !newPanel.open,
+    });
+  }
+
   return (
     <>
       <DialogTitle>
@@ -135,97 +157,41 @@ export default function IncomeDialog({ incomeDialogOpen, setIncomeDialogOpen }: 
               </Field>
             </div>
             <Disclosure as="div" className="border-border/50 border-y py-4">
-              <DisclosureButton className="group data-open:border-border/25 focus-outline flex w-full items-start justify-between text-left data-open:border-b data-open:pb-4">
-                <div className="flex items-center gap-2">
-                  <CalendarIcon className="text-primary size-5 shrink-0" aria-hidden="true" />
-                  <span className="text-base/7 font-semibold">Timeframe</span>
-                </div>
-                <span className="text-muted-foreground ml-6 flex h-7 items-center">
-                  <PlusIcon aria-hidden="true" className="size-6 group-data-open:hidden" />
-                  <MinusIcon aria-hidden="true" className="size-6 group-not-data-open:hidden" />
-                </span>
-              </DisclosureButton>
-              <DisclosurePanel className="py-4">
-                <div className="grid grid-cols-3 gap-4">
-                  <Field className={getStartColSpan()}>
-                    <Label htmlFor="start">Start</Label>
-                    <Select {...register('timeframe.start.type')} id="start" name="timeframe.start.type">
-                      <option value="now">Now</option>
-                      <option value="at-retirement">At Retirement</option>
-                      <option value="custom-date">Custom Date</option>
-                      <option value="custom-age">Custom Age</option>
-                    </Select>
-                  </Field>
-                  {startType === 'custom-date' && (
-                    <>
-                      <Field>
-                        <Label>Month</Label>
-                        <Controller
-                          name="timeframe.start.month"
-                          defaultValue={currentMonth.value}
-                          control={control}
-                          render={({ field: { onChange, value, name } }) => (
-                            <Combobox
-                              name={name}
-                              options={months}
-                              displayValue={(month) => month?.name || currentMonth.name}
-                              value={months.find((m) => m.value === value) || currentMonth}
-                              onChange={(month) => onChange(month?.value || currentMonth.value)}
-                              filter={(month, query) =>
-                                month.name.toLowerCase().includes(query.toLowerCase()) || String(month.value).includes(query)
-                              }
-                            >
-                              {(month) => (
-                                <ComboboxOption value={month}>
-                                  <ComboboxLabel>{month.name}</ComboboxLabel>
-                                </ComboboxOption>
-                              )}
-                            </Combobox>
-                          )}
-                        />
-                      </Field>
-                      <Field>
-                        <Label>Year</Label>
-                        <Controller
-                          name="timeframe.start.year"
-                          defaultValue={currentYear}
-                          control={control}
-                          render={({ field: { onChange, value, name } }) => (
-                            <Combobox
-                              name={name}
-                              options={years}
-                              displayValue={(year) => String(year || currentYear)}
-                              value={value || currentYear}
-                              onChange={(year) => onChange(year || currentYear)}
-                            >
-                              {(year) => (
-                                <ComboboxOption value={year}>
-                                  <ComboboxLabel>{year}</ComboboxLabel>
-                                </ComboboxOption>
-                              )}
-                            </Combobox>
-                          )}
-                        />
-                      </Field>
-                    </>
-                  )}
-                  {frequency !== 'one-time' && (
-                    <>
-                      <Field className={getEndColSpan()}>
-                        <Label htmlFor="end">End</Label>
-                        <Select {...register('timeframe.end.type')} id="end" name="timeframe.end.type">
+              {(panel) => (
+                <>
+                  <DisclosureButton
+                    onClick={() => {
+                      if (!open) close();
+                      togglePanels({ ...panel, key: 'timeframe' });
+                    }}
+                    className="group data-open:border-border/25 focus-outline flex w-full items-start justify-between text-left data-open:border-b data-open:pb-4"
+                  >
+                    <div className="flex items-center gap-2">
+                      <CalendarIcon className="text-primary size-5 shrink-0" aria-hidden="true" />
+                      <span className="text-base/7 font-semibold">Timeframe</span>
+                    </div>
+                    <span className="text-muted-foreground ml-6 flex h-7 items-center">
+                      <PlusIcon aria-hidden="true" className="size-6 group-data-open:hidden" />
+                      <MinusIcon aria-hidden="true" className="size-6 group-not-data-open:hidden" />
+                    </span>
+                  </DisclosureButton>
+                  <DisclosurePanel className="py-4">
+                    <div className="grid grid-cols-3 gap-4">
+                      <Field className={getStartColSpan()}>
+                        <Label htmlFor="start">Start</Label>
+                        <Select {...register('timeframe.start.type')} id="start" name="timeframe.start.type">
+                          <option value="now">Now</option>
                           <option value="at-retirement">At Retirement</option>
-                          <option value="at-life-expectancy">At Life Expectancy</option>
                           <option value="custom-date">Custom Date</option>
                           <option value="custom-age">Custom Age</option>
                         </Select>
                       </Field>
-                      {endType === 'custom-date' && (
+                      {startType === 'custom-date' && (
                         <>
                           <Field>
                             <Label>Month</Label>
                             <Controller
-                              name="timeframe.end.month"
+                              name="timeframe.start.month"
                               defaultValue={currentMonth.value}
                               control={control}
                               render={({ field: { onChange, value, name } }) => (
@@ -251,7 +217,7 @@ export default function IncomeDialog({ incomeDialogOpen, setIncomeDialogOpen }: 
                           <Field>
                             <Label>Year</Label>
                             <Controller
-                              name="timeframe.end.year"
+                              name="timeframe.start.year"
                               defaultValue={currentYear}
                               control={control}
                               render={({ field: { onChange, value, name } }) => (
@@ -273,53 +239,129 @@ export default function IncomeDialog({ incomeDialogOpen, setIncomeDialogOpen }: 
                           </Field>
                         </>
                       )}
-                    </>
-                  )}
-                </div>
-              </DisclosurePanel>
+                      {frequency !== 'one-time' && (
+                        <>
+                          <Field className={getEndColSpan()}>
+                            <Label htmlFor="end">End</Label>
+                            <Select {...register('timeframe.end.type')} id="end" name="timeframe.end.type">
+                              <option value="at-retirement">At Retirement</option>
+                              <option value="at-life-expectancy">At Life Expectancy</option>
+                              <option value="custom-date">Custom Date</option>
+                              <option value="custom-age">Custom Age</option>
+                            </Select>
+                          </Field>
+                          {endType === 'custom-date' && (
+                            <>
+                              <Field>
+                                <Label>Month</Label>
+                                <Controller
+                                  name="timeframe.end.month"
+                                  defaultValue={currentMonth.value}
+                                  control={control}
+                                  render={({ field: { onChange, value, name } }) => (
+                                    <Combobox
+                                      name={name}
+                                      options={months}
+                                      displayValue={(month) => month?.name || currentMonth.name}
+                                      value={months.find((m) => m.value === value) || currentMonth}
+                                      onChange={(month) => onChange(month?.value || currentMonth.value)}
+                                      filter={(month, query) =>
+                                        month.name.toLowerCase().includes(query.toLowerCase()) || String(month.value).includes(query)
+                                      }
+                                    >
+                                      {(month) => (
+                                        <ComboboxOption value={month}>
+                                          <ComboboxLabel>{month.name}</ComboboxLabel>
+                                        </ComboboxOption>
+                                      )}
+                                    </Combobox>
+                                  )}
+                                />
+                              </Field>
+                              <Field>
+                                <Label>Year</Label>
+                                <Controller
+                                  name="timeframe.end.year"
+                                  defaultValue={currentYear}
+                                  control={control}
+                                  render={({ field: { onChange, value, name } }) => (
+                                    <Combobox
+                                      name={name}
+                                      options={years}
+                                      displayValue={(year) => String(year || currentYear)}
+                                      value={value || currentYear}
+                                      onChange={(year) => onChange(year || currentYear)}
+                                    >
+                                      {(year) => (
+                                        <ComboboxOption value={year}>
+                                          <ComboboxLabel>{year}</ComboboxLabel>
+                                        </ComboboxOption>
+                                      )}
+                                    </Combobox>
+                                  )}
+                                />
+                              </Field>
+                            </>
+                          )}
+                        </>
+                      )}
+                    </div>
+                  </DisclosurePanel>
+                </>
+              )}
             </Disclosure>
             <Disclosure as="div">
-              <DisclosureButton className="group data-open:border-border/25 focus-outline flex w-full items-start justify-between text-left data-open:border-b data-open:pb-4">
-                <div className="flex items-center gap-2">
-                  <ArrowTrendingUpIcon className="text-primary size-5 shrink-0" aria-hidden="true" />
-                  <span className="text-base/7 font-semibold">Rate of Change</span>
-                </div>
-                <span className="text-muted-foreground ml-6 flex h-7 items-center">
-                  <PlusIcon aria-hidden="true" className="size-6 group-data-open:hidden" />
-                  <MinusIcon aria-hidden="true" className="size-6 group-not-data-open:hidden" />
-                </span>
-              </DisclosureButton>
-              <DisclosurePanel className="py-4">
-                <div className="grid grid-cols-5 gap-4">
-                  <Field className="col-span-3">
-                    <Label htmlFor="growth-rate" className="flex w-full items-center justify-between">
-                      <span>Growth Rate</span>
-                      <span className="text-muted-foreground text-sm/6">{Number(3).toFixed(1)}% real</span>
-                    </Label>
-                    <NumberInputV2
-                      name="growth.growthRate"
-                      control={control}
-                      id="growth-rate"
-                      inputMode="decimal"
-                      placeholder="3%"
-                      suffix="%"
-                    />
-                    {errors.growth?.growthRate && <ErrorMessage>{errors.growth?.growthRate?.message}</ErrorMessage>}
-                  </Field>
-                  <Field className="col-span-2">
-                    <Label htmlFor="growth-limit">Limit</Label>
-                    <NumberInputV2
-                      name="growth.growthLimit"
-                      control={control}
-                      id="growth-limit"
-                      inputMode="decimal"
-                      placeholder="$120,000"
-                      prefix="$"
-                    />
-                    {errors.growth?.growthLimit && <ErrorMessage>{errors.growth?.growthLimit?.message}</ErrorMessage>}
-                  </Field>
-                </div>
-              </DisclosurePanel>
+              {(panel) => (
+                <>
+                  <DisclosureButton
+                    onClick={() => {
+                      if (!open) close();
+                      togglePanels({ ...panel, key: 'rateOfChange' });
+                    }}
+                    className="group data-open:border-border/25 focus-outline flex w-full items-start justify-between text-left data-open:border-b data-open:pb-4"
+                  >
+                    <div className="flex items-center gap-2">
+                      <ArrowTrendingUpIcon className="text-primary size-5 shrink-0" aria-hidden="true" />
+                      <span className="text-base/7 font-semibold">Rate of Change</span>
+                    </div>
+                    <span className="text-muted-foreground ml-6 flex h-7 items-center">
+                      <PlusIcon aria-hidden="true" className="size-6 group-data-open:hidden" />
+                      <MinusIcon aria-hidden="true" className="size-6 group-not-data-open:hidden" />
+                    </span>
+                  </DisclosureButton>
+                  <DisclosurePanel className="py-4">
+                    <div className="grid grid-cols-5 gap-4">
+                      <Field className="col-span-3">
+                        <Label htmlFor="growth-rate" className="flex w-full items-center justify-between">
+                          <span>Growth Rate</span>
+                          <span className="text-muted-foreground text-sm/6">{Number(3).toFixed(1)}% real</span>
+                        </Label>
+                        <NumberInputV2
+                          name="growth.growthRate"
+                          control={control}
+                          id="growth-rate"
+                          inputMode="decimal"
+                          placeholder="3%"
+                          suffix="%"
+                        />
+                        {errors.growth?.growthRate && <ErrorMessage>{errors.growth?.growthRate?.message}</ErrorMessage>}
+                      </Field>
+                      <Field className="col-span-2">
+                        <Label htmlFor="growth-limit">Limit</Label>
+                        <NumberInputV2
+                          name="growth.growthLimit"
+                          control={control}
+                          id="growth-limit"
+                          inputMode="decimal"
+                          placeholder="$120,000"
+                          prefix="$"
+                        />
+                        {errors.growth?.growthLimit && <ErrorMessage>{errors.growth?.growthLimit?.message}</ErrorMessage>}
+                      </Field>
+                    </div>
+                  </DisclosurePanel>
+                </>
+              )}
             </Disclosure>
           </DialogBody>
         </Fieldset>
