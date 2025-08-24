5b99a82cd3d7431d62c53906947d359a8a93fabf
schelskedevco <joe@schelske.dev>
2025-08-23 21:33:07 -0500

    account dialog initial setup

--- src/app/dashboard/quick-plan/components/inputs/dialogs/account-dialog.tsx
+++ src/app/dashboard/quick-plan/components/inputs/dialogs/account-dialog.tsx
@@ -5,16 +5,17 @@ import { LandmarkIcon } from 'lucide-react';
 // import { v4 as uuidv4 } from 'uuid';
 // import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
 // import { MinusIcon, PlusIcon, ArrowTrendingUpIcon } from '@heroicons/react/24/outline';
-// import { zodResolver } from '@hookform/resolvers/zod';
-// import { useForm, useWatch, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm /* useWatch, Controller */ } from 'react-hook-form';
 
 import { DialogTitle, DialogBody, DialogActions } from '@/components/catalyst/dialog';
-// import NumberInputV2 from '@/components/ui/number-input-v2';
-import { Fieldset /* Field, Label, ErrorMessage  Description */ } from '@/components/catalyst/fieldset';
+import { accountFormSchema, type AccountInputs } from '@/lib/schemas/account-form-schema';
+import NumberInputV2 from '@/components/ui/number-input-v2';
+import { Fieldset, Field, Label, ErrorMessage } from '@/components/catalyst/fieldset';
 // import { Combobox, ComboboxLabel, ComboboxOption } from '@/components/catalyst/combobox';
-// import { Select } from '@/components/catalyst/select';
+import { Select } from '@/components/catalyst/select';
 import { Button } from '@/components/catalyst/button';
-// import { Input } from '@/components/catalyst/input';
+import { Input } from '@/components/catalyst/input';
 
 interface AccountDialogProps {
   setAccountDialogOpen: (open: boolean) => void;
@@ -22,6 +23,19 @@ interface AccountDialogProps {
 }
 
 export default function AccountDialog({ setAccountDialogOpen, selectedAccountID }: AccountDialogProps) {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(accountFormSchema),
+  });
+
+  const onSubmit = (data: AccountInputs) => {
+    console.log(data);
+  };
+
   return (
     <>
       <DialogTitle>
@@ -30,10 +44,51 @@ export default function AccountDialog({ setAccountDialogOpen, selectedAccountID 
           <span>New Account</span>
         </div>
       </DialogTitle>
-      <form onSubmit={() => {}}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Fieldset aria-label="Account details">
           <DialogBody data-slot="control" className="space-y-4">
-            <p>Placeholder Text.</p>
+            <div className="mb-8 grid grid-cols-2 gap-4">
+              <Field className="col-span-2">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  {...register('name')}
+                  id="name"
+                  name="name"
+                  placeholder="My Salary"
+                  autoComplete="off"
+                  inputMode="text"
+                  invalid={!!errors.name}
+                  aria-invalid={!!errors.name}
+                  autoFocus={selectedAccountID === null}
+                />
+                {errors.name && <ErrorMessage>{errors.name?.message}</ErrorMessage>}
+              </Field>
+              <Field>
+                <Label htmlFor="balance">Balance</Label>
+                <NumberInputV2
+                  name="balance"
+                  control={control}
+                  id="balance"
+                  inputMode="decimal"
+                  placeholder="$85,000"
+                  prefix="$"
+                  autoFocus={selectedAccountID !== null}
+                />
+                {errors.balance && <ErrorMessage>{errors.balance?.message}</ErrorMessage>}
+              </Field>
+              <Field>
+                <Label htmlFor="type">Account Type</Label>
+                <Select {...register('type')} id="type" name="type">
+                  <option value="savings">Savings</option>
+                  <option value="taxable-brokerage">Taxable Brokerage</option>
+                  <option value="401k">401(k)</option>
+                  <option value="ira">IRA</option>
+                  <option value="roth-401k">Roth 401(k)</option>
+                  <option value="roth-ira">Roth IRA</option>
+                  <option value="hsa">HSA</option>
+                </Select>
+              </Field>
+            </div>
           </DialogBody>
         </Fieldset>
         <DialogActions>
