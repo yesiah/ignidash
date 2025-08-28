e605a01df709b0280954323960ae0609db71cbe1
schelskedevco <joe@schelske.dev>
2025-08-28 01:43:29 -0500

    remove gradient

--- src/app/dashboard/quick-plan/page.tsx
+++ src/app/dashboard/quick-plan/page.tsx
@@ -15,15 +15,8 @@ export default function QuickPlanPage() {
       </MainArea>
       <SecondaryColumn>
         <NumbersColumnHeader />
-        <div className="relative flex h-[calc(100%-4.3125rem)] flex-col">
+        <div className="flex h-[calc(100%-4.3125rem)] flex-col">
           <NumbersColumnSections />
-          <div
-            className="pointer-events-none absolute inset-x-0 bottom-0 -z-10 h-40 animate-pulse"
-            style={{
-              background: 'radial-gradient(ellipse 200px 160px at center bottom, rgb(244 63 94 / 0.3) 0%, transparent 75%)',
-              animationDuration: '3s',
-            }}
-          />
         </div>
       </SecondaryColumn>
     </>
