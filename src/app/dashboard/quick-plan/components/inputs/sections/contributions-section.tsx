f961a713552a87064c07480314f224635b7c5dba
schelskedevco <joe@schelske.dev>
2025-08-29 00:13:40 -0500

    contribution section data item

--- src/app/dashboard/quick-plan/components/inputs/sections/contributions-section.tsx
+++ src/app/dashboard/quick-plan/components/inputs/sections/contributions-section.tsx
@@ -2,7 +2,6 @@
 
 import { useState, RefObject } from 'react';
 import { HandCoinsIcon, PiggyBankIcon, BanknoteArrowDownIcon } from 'lucide-react';
-import { EllipsisVerticalIcon } from '@heroicons/react/20/solid';
 import { PlusIcon } from '@heroicons/react/16/solid';
 
 import DisclosureSection from '@/components/ui/disclosure-section';
@@ -10,16 +9,13 @@ import { Dialog } from '@/components/catalyst/dialog';
 import { Button } from '@/components/catalyst/button';
 import { DisclosureState } from '@/lib/types/disclosure-state';
 import { Divider } from '@/components/catalyst/divider';
-import { Dropdown, DropdownButton, DropdownItem, DropdownMenu } from '@/components/catalyst/dropdown';
 import { Alert, AlertActions, AlertDescription, AlertTitle } from '@/components/catalyst/alert';
 import { Field, Label, Description } from '@/components/catalyst/fieldset';
 import { Listbox, ListboxLabel, ListboxDescription, ListboxOption } from '@/components/catalyst/listbox';
-import { cn } from '@/lib/utils';
 import { useContributionRulesData, useDeleteContributionRule } from '@/lib/stores/quick-plan-store';
 
 import ContributionRuleDialog from '../dialogs/contribution-rule-dialog';
-
-const colors = ['bg-rose-500/50', 'bg-rose-500/75', 'bg-rose-500'];
+import DisclosureSectionDataItem from '../disclosure-section-data-item';
 
 interface ContributionsSectionProps {
   toggleDisclosure: (newDisclosure: DisclosureState) => void;
@@ -53,48 +49,21 @@ export default function ContributionsSection({ toggleDisclosure, disclosureButto
             <div className="flex h-full flex-col">
               <ul role="list" className="mb-6 grid grid-cols-1 gap-3">
                 {Object.entries(contributionRules).map(([id, contributionRule], index) => (
-                  <li key={id} className="col-span-1 flex rounded-md shadow-xs dark:shadow-none">
-                    <div
-                      className={cn(
-                        'border-foreground/50 flex w-16 shrink-0 items-center justify-center rounded-l-md border text-xl font-medium text-white',
-                        colors[index % colors.length]
-                      )}
-                    >
-                      {contributionRule.rank}
-                    </div>
-                    <div className="bg-emphasized-background/25 border-border flex flex-1 items-center justify-between truncate rounded-r-md border-t border-r border-b">
-                      <div className="flex-1 truncate px-4 py-2 text-sm">
-                        <span className="font-medium text-gray-900 hover:text-gray-600 dark:text-white dark:hover:text-gray-200">
-                          {'Contribution Rule ' + (index + 1)}
-                        </span>
-                        <p className="text-muted-foreground">Placeholder Contribution Rule Description</p>
-                      </div>
-                      <div className="shrink-0 pr-2">
-                        <Dropdown>
-                          <DropdownButton plain aria-label="Open options">
-                            <EllipsisVerticalIcon />
-                          </DropdownButton>
-                          <DropdownMenu>
-                            <DropdownItem
-                              onClick={() => {
-                                setContributionRuleDialogOpen(true);
-                                setSelectedContributionRuleID(id);
-                              }}
-                            >
-                              Edit
-                            </DropdownItem>
-                            <DropdownItem
-                              onClick={() => {
-                                setContributionRuleToDelete({ id, name: 'Contribution Rule ' + (index + 1) });
-                              }}
-                            >
-                              Delete
-                            </DropdownItem>
-                          </DropdownMenu>
-                        </Dropdown>
-                      </div>
-                    </div>
-                  </li>
+                  <DisclosureSectionDataItem
+                    key={id}
+                    id={id}
+                    index={index}
+                    name={'Contribution Rule ' + (index + 1)}
+                    desc={'Placeholder Contribution Rule Description'}
+                    leftAddOnCharacter={String(contributionRule.rank)}
+                    onDropdownClickEdit={() => {
+                      setContributionRuleDialogOpen(true);
+                      setSelectedContributionRuleID(id);
+                    }}
+                    onDropdownClickDelete={() => {
+                      setContributionRuleToDelete({ id, name: 'Contribution Rule ' + (index + 1) });
+                    }}
+                  />
                 ))}
               </ul>
               <div className="mt-auto flex items-center justify-end">
