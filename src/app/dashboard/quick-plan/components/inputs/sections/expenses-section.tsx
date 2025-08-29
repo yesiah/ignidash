e179d17971191242581de752da5d4dd018f579df
schelskedevco <joe@schelske.dev>
2025-08-29 00:10:33 -0500

    expense

--- src/app/dashboard/quick-plan/components/inputs/sections/expenses-section.tsx
+++ src/app/dashboard/quick-plan/components/inputs/sections/expenses-section.tsx
@@ -2,21 +2,18 @@
 
 import { useState, RefObject } from 'react';
 import { BanknoteArrowDownIcon } from 'lucide-react';
-import { EllipsisVerticalIcon } from '@heroicons/react/20/solid';
 import { PlusIcon } from '@heroicons/react/16/solid';
 
 import DisclosureSection from '@/components/ui/disclosure-section';
 import { Dialog } from '@/components/catalyst/dialog';
 import { Button } from '@/components/catalyst/button';
-import { Dropdown, DropdownButton, DropdownItem, DropdownMenu } from '@/components/catalyst/dropdown';
 import { Alert, AlertActions, AlertDescription, AlertTitle } from '@/components/catalyst/alert';
 import { useExpensesData, useDeleteExpense } from '@/lib/stores/quick-plan-store';
-import { cn, formatNumber } from '@/lib/utils';
+import { formatNumber } from '@/lib/utils';
 import type { DisclosureState } from '@/lib/types/disclosure-state';
 
 import ExpenseDialog from '../dialogs/expense-dialog';
-
-const colors = ['bg-rose-500/50', 'bg-rose-500/75', 'bg-rose-500'];
+import DisclosureSectionDataItem from '../disclosure-section-data-item';
 
 interface ExpensesSectionProps {
   toggleDisclosure: (newDisclosure: DisclosureState) => void;
@@ -49,48 +46,21 @@ export default function ExpensesSection({ toggleDisclosure, disclosureButtonRef,
           <div className="flex h-full flex-col">
             <ul role="list" className="mb-6 grid grid-cols-1 gap-3">
               {Object.entries(expenses).map(([id, expense], index) => (
-                <li key={id} className="col-span-1 flex rounded-md shadow-xs dark:shadow-none">
-                  <div
-                    className={cn(
-                      'border-foreground/50 flex w-16 shrink-0 items-center justify-center rounded-l-md border text-xl font-medium text-white',
-                      colors[index % colors.length]
-                    )}
-                  >
-                    {expense.name.charAt(0).toUpperCase()}
-                  </div>
-                  <div className="bg-emphasized-background/25 border-border flex flex-1 items-center justify-between truncate rounded-r-md border-t border-r border-b">
-                    <div className="flex-1 truncate px-4 py-2 text-sm">
-                      <span className="font-medium text-gray-900 hover:text-gray-600 dark:text-white dark:hover:text-gray-200">
-                        {expense.name}
-                      </span>
-                      <p className="text-muted-foreground">{formatNumber(expense.amount, 2, '$') + ` ${expense.frequency}`}</p>
-                    </div>
-                    <div className="shrink-0 pr-2">
-                      <Dropdown>
-                        <DropdownButton plain aria-label="Open options">
-                          <EllipsisVerticalIcon />
-                        </DropdownButton>
-                        <DropdownMenu>
-                          <DropdownItem
-                            onClick={() => {
-                              setExpenseDialogOpen(true);
-                              setSelectedExpenseID(id);
-                            }}
-                          >
-                            Edit
-                          </DropdownItem>
-                          <DropdownItem
-                            onClick={() => {
-                              setExpenseToDelete({ id, name: expense.name });
-                            }}
-                          >
-                            Delete
-                          </DropdownItem>
-                        </DropdownMenu>
-                      </Dropdown>
-                    </div>
-                  </div>
-                </li>
+                <DisclosureSectionDataItem
+                  key={id}
+                  id={id}
+                  index={index}
+                  name={expense.name}
+                  desc={formatNumber(expense.amount, 2, '$') + ` ${expense.frequency}`}
+                  leftAddOnCharacter={expense.name.charAt(0).toUpperCase()}
+                  onDropdownClickEdit={() => {
+                    setExpenseDialogOpen(true);
+                    setSelectedExpenseID(id);
+                  }}
+                  onDropdownClickDelete={() => {
+                    setExpenseToDelete({ id, name: expense.name });
+                  }}
+                />
               ))}
             </ul>
             <div className="mt-auto flex items-center justify-end">
