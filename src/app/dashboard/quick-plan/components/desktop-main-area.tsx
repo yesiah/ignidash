58fe0412c1337cae05da6c7e7cc59ee6548644f7
schelskedevco <joe@schelske.dev>
2025-08-31 12:09:56 -0500

    h-full

--- src/app/dashboard/quick-plan/components/desktop-main-area.tsx
+++ src/app/dashboard/quick-plan/components/desktop-main-area.tsx
@@ -5,7 +5,7 @@ export default function DesktopMainArea() {
   return (
     <div className="hidden xl:block">
       <ResultsColumnHeader />
-      <div className="flex h-[calc(100%-4.3125rem)] flex-col pt-[4.3125rem]">
+      <div className="flex h-full flex-col pt-[4.3125rem]">
         <ResultsSections />
       </div>
     </div>
