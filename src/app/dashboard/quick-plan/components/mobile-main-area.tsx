c03bf96aec2d4bf844d182760a575aa3fd321046
schelskedevco <joe@schelske.dev>
2025-08-31 11:46:28 -0500

    fixed instead of sticky for mobile

--- src/app/dashboard/quick-plan/components/mobile-main-area.tsx
+++ src/app/dashboard/quick-plan/components/mobile-main-area.tsx
@@ -22,7 +22,7 @@ export default function MobileMainArea() {
   }
 
   return (
-    <div className="flex h-full flex-col xl:hidden">
+    <div className="mt-[7.375rem] flex h-[calc(100%-7.375rem)] flex-col lg:mt-[4.3125rem] lg:h-[calc(100%-4.3125rem)] xl:hidden">
       <SectionSelector activeSection={activeSection} setActiveSection={setActiveSection} />
       {activeSectionComponent}
     </div>
