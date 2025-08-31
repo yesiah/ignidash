dc9aa19b86df3b12f182ab74006cdfbe8f5bde40
schelskedevco <joe@schelske.dev>
2025-08-31 12:13:04 -0500

    h-full

--- src/app/dashboard/quick-plan/components/mobile-main-area.tsx
+++ src/app/dashboard/quick-plan/components/mobile-main-area.tsx
@@ -22,7 +22,7 @@ export default function MobileMainArea() {
   }
 
   return (
-    <div className="flex h-[calc(100%-7.375rem)] flex-col pt-[7.375rem] lg:h-[calc(100%-4.3125rem)] lg:pt-[4.3125rem] xl:hidden">
+    <div className="flex h-full flex-col pt-[7.375rem] lg:pt-[4.3125rem] xl:hidden">
       <SectionSelector activeSection={activeSection} setActiveSection={setActiveSection} />
       {activeSectionComponent}
     </div>
