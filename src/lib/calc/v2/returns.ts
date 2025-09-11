ae4a86f0b79aca6cdfd1de7c5606e9a14e09a491
schelskedevco <joe@schelske.dev>
2025-09-11 18:14:40 -0500

    renmame stuff

--- src/lib/calc/v2/returns.ts
+++ src/lib/calc/v2/returns.ts
@@ -4,9 +4,9 @@ import type { AssetReturnRates, AssetReturnAmounts } from '../asset';
 
 export interface ReturnsData {
   // Monthly return data
-  monthlyReturnAmounts: AssetReturnAmounts;
-  monthlyReturnRates: AssetReturnRates;
-  monthlyInflationRate: number;
+  returnAmountsForPeriod: AssetReturnAmounts;
+  returnRatesForPeriod: AssetReturnRates;
+  inflationRateForPeriod: number;
 
   // Annual return data
   annualReturnRates: AssetReturnRates;
@@ -40,19 +40,19 @@ export class ReturnsProcessor {
       this.lastYear = currentYear;
     }
 
-    const monthlyReturnRates: AssetReturnRates = {
+    const returnRatesForPeriod: AssetReturnRates = {
       stocks: Math.pow(1 + this.cachedAnnualReturnRates.stocks, 1 / 12) - 1,
       bonds: Math.pow(1 + this.cachedAnnualReturnRates.bonds, 1 / 12) - 1,
       cash: Math.pow(1 + this.cachedAnnualReturnRates.cash, 1 / 12) - 1,
     };
-    const monthlyInflationRate = Math.pow(1 + this.cachedAnnualInflationRate, 1 / 12) - 1;
+    const inflationRateForPeriod = Math.pow(1 + this.cachedAnnualInflationRate, 1 / 12) - 1;
 
-    const monthlyReturnAmounts = this.simulationState.portfolio.applyReturns(monthlyReturnRates);
+    const returnAmountsForPeriod = this.simulationState.portfolio.applyReturns(returnRatesForPeriod);
 
     const result = {
-      monthlyReturnAmounts,
-      monthlyReturnRates,
-      monthlyInflationRate,
+      returnAmountsForPeriod,
+      returnRatesForPeriod,
+      inflationRateForPeriod,
       annualReturnRates: this.cachedAnnualReturnRates,
       annualInflationRate: this.cachedAnnualInflationRate,
     };
@@ -71,12 +71,12 @@ export class ReturnsProcessor {
 
   getAnnualData(): ReturnsData {
     return {
-      ...this.monthlyData[0] /* Only need to accumulate monthlyReturnAmounts */,
-      monthlyReturnAmounts: this.monthlyData.reduce(
+      ...this.monthlyData[0] /* Only need to accumulate returnAmounts */,
+      returnAmountsForPeriod: this.monthlyData.reduce(
         (acc, curr) => ({
-          stocks: acc.stocks + curr.monthlyReturnAmounts.stocks,
-          bonds: acc.bonds + curr.monthlyReturnAmounts.bonds,
-          cash: acc.cash + curr.monthlyReturnAmounts.cash,
+          stocks: acc.stocks + curr.returnAmountsForPeriod.stocks,
+          bonds: acc.bonds + curr.returnAmountsForPeriod.bonds,
+          cash: acc.cash + curr.returnAmountsForPeriod.cash,
         }),
         { stocks: 0, bonds: 0, cash: 0 }
       ),
