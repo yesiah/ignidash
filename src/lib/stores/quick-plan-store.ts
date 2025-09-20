3ba7ffeefc06dc8a8cd3d6af65597d358f5e8627
schelskedevco <joe@schelske.dev>
2025-09-20 11:21:59 -0500

    iterate over the new state.inputs.contributionRules

--- src/lib/stores/quick-plan-store.ts
+++ src/lib/stores/quick-plan-store.ts
@@ -189,10 +189,10 @@ export const useQuickPlanStore = create<QuickPlanState>()(
               const contributionRules = Object.values(state.inputs.contributionRules);
               const contributionRuleToDelete = contributionRules.find((rule) => rule.accountId === id);
               if (contributionRuleToDelete !== undefined) {
+                const deletedRank = contributionRuleToDelete.rank;
                 delete state.inputs.contributionRules[contributionRuleToDelete.id];
 
-                const deletedRank = contributionRuleToDelete.rank;
-                contributionRules.forEach((rule) => {
+                Object.values(state.inputs.contributionRules).forEach((rule) => {
                   if (rule.rank > deletedRank) rule.rank--;
                 });
               }
