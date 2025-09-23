fecbd9cc0a1aad6d68dbfd1dd9f57760d1a2d322
schelskedevco <joe@schelske.dev>
2025-09-23 12:33:16 -0500

    grid cols

--- src/app/dashboard/quick-plan/components/outputs/sections/single-simulation-data-list-section.tsx
+++ src/app/dashboard/quick-plan/components/outputs/sections/single-simulation-data-list-section.tsx
@@ -4,6 +4,7 @@ import { Subheading } from '@/components/catalyst/heading';
 import { formatNumber } from '@/lib/utils';
 import SectionHeader from '@/components/ui/section-header';
 import SectionContainer from '@/components/ui/section-container';
+import Card from '@/components/ui/card';
 
 interface SingleSimulationDataListSectionProps {
   simulation: SimulationResult;
@@ -13,14 +14,18 @@ export default function SingleSimulationDataListSection({ simulation }: SingleSi
   return (
     <SectionContainer showBottomBorder>
       <SectionHeader title={'Data List'} desc={'Data List Desc'} className="mb-4" />
-      <Subheading level={4}>Order #1011</Subheading>
-      <DescriptionList>
-        <DescriptionTerm>Item 1</DescriptionTerm>
-        <DescriptionDetails>{formatNumber(1000000, 2, '$')}</DescriptionDetails>
+      <div className="grid grid-cols-1 gap-2 @3xl:grid-cols-2 @5xl:grid-cols-3">
+        <Card>
+          <Subheading level={4}>Order #1011</Subheading>
+          <DescriptionList>
+            <DescriptionTerm>Item 1</DescriptionTerm>
+            <DescriptionDetails>{formatNumber(1000000, 2, '$')}</DescriptionDetails>
 
-        <DescriptionTerm>Item 2</DescriptionTerm>
-        <DescriptionDetails>{formatNumber(1000000, 2, '$')}</DescriptionDetails>
-      </DescriptionList>
+            <DescriptionTerm>Item 2</DescriptionTerm>
+            <DescriptionDetails>{formatNumber(1000000, 2, '$')}</DescriptionDetails>
+          </DescriptionList>
+        </Card>
+      </div>
     </SectionContainer>
   );
 }
