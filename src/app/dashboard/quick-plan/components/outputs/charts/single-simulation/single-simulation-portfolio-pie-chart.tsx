075d3c40df84fc6cf22586fd98630cff4b87659d
schelskedevco <joe@schelske.dev>
2025-09-24 11:31:11 -0500

    14

--- src/app/dashboard/quick-plan/components/outputs/charts/single-simulation/single-simulation-portfolio-pie-chart.tsx
+++ src/app/dashboard/quick-plan/components/outputs/charts/single-simulation/single-simulation-portfolio-pie-chart.tsx
@@ -57,7 +57,7 @@ const createRenderActiveShape = (isSmallScreen: boolean) => {
           fill="currentColor"
           fontSize="14"
         >{`${value !== undefined ? formatNumber(value, 2, '$') : 'N/A'}`}</text>
-        <text x={cx} y={cy} dy={0} textAnchor="middle" fill="currentColor" fontSize={isSmallScreen ? '14' : '16'} fontWeight="600">
+        <text x={cx} y={cy} dy={0} textAnchor="middle" fill="currentColor" fontSize="14" fontWeight="600">
           {formatChartString(payload?.name ?? 'N/A')}
         </text>
         <text x={cx} y={cy} dy={20} textAnchor="middle" fill="var(--muted-foreground)" fontSize="14">
