4f1d8f489a3a326298ce9958077a8df8fb2f19ad
schelskedevco <joe@schelske.dev>
2025-10-04 16:38:23 -0500

    circles

--- src/app/dashboard/quick-plan/components/outputs/metrics-card.tsx
+++ src/app/dashboard/quick-plan/components/outputs/metrics-card.tsx
@@ -15,6 +15,9 @@ export default function MetricsCard({ name, stat, statContext, className }: Metr
       <dd className="text-foreground mt-1 text-3xl font-semibold tracking-tight">
         <span>{stat}</span>
         <span className="text-muted-foreground ml-1 text-sm">{statContext}</span>
+        {/* <svg viewBox="0 0 18 18" aria-hidden="true" className="inline size-4.5 fill-green-500 dark:fill-green-400">
+          <circle r={9} cx={9} cy={9} />
+        </svg> */}
       </dd>
     </Card>
   );
