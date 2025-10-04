8817581056e223de13ae1255a8a3a8dc7d375fb1
schelskedevco <joe@schelske.dev>
2025-10-04 16:17:06 -0500

    remove attribution

--- src/app/dashboard/quick-plan/components/desktop-main-area.tsx
+++ src/app/dashboard/quick-plan/components/desktop-main-area.tsx
@@ -1,6 +1,6 @@
 import ResultsSections from './outputs/results-sections';
 import ResultsColumnHeader from './outputs/results-column-header';
-import Icons8AttributionLink from './outputs/icons8-attribution-link';
+// import Icons8AttributionLink from './outputs/icons8-attribution-link';
 
 export default function DesktopMainArea() {
   return (
@@ -8,7 +8,7 @@ export default function DesktopMainArea() {
       <ResultsColumnHeader />
       <div className="flex h-full flex-col pt-[4.3125rem]">
         <ResultsSections />
-        <Icons8AttributionLink />
+        {/* <Icons8AttributionLink /> */}
       </div>
     </div>
   );
