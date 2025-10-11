53b211acce5e64f153732e0c54d8f7508699004a
schelskedevco <joe@schelske.dev>
2025-10-11 15:04:59 -0500

    gross income

--- src/app/dashboard/quick-plan/components/outputs/sections/single-simulation-data-list-section.tsx
+++ src/app/dashboard/quick-plan/components/outputs/sections/single-simulation-data-list-section.tsx
@@ -147,7 +147,7 @@ function TaxesDataListCardV2({ dp, selectedAge }: DataListCardProps) {
         <span className="text-muted-foreground hidden sm:inline">Age {selectedAge}</span>
       </Subheading>
       <DescriptionList>
-        <DescriptionTerm>Gross Income*</DescriptionTerm>
+        <DescriptionTerm>Gross Income</DescriptionTerm>
         <DescriptionDetails>{formatNumber(grossIncome, 2, '$')}</DescriptionDetails>
 
         <DescriptionTerm>Taxable Retirement Distributions</DescriptionTerm>
