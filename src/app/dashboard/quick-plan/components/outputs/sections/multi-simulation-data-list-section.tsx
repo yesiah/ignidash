b661fe85783ebe0904e22e304e7ff8efb8285a77
schelskedevco <joe@schelske.dev>
2025-10-18 02:26:31 -0500

    language tweak

--- src/app/dashboard/quick-plan/components/outputs/sections/multi-simulation-data-list-section.tsx
+++ src/app/dashboard/quick-plan/components/outputs/sections/multi-simulation-data-list-section.tsx
@@ -59,7 +59,7 @@ function PhasesDataListCardV2({ chartData, selectedAge }: DataListCardProps) {
     <Card className="my-0">
       <Subheading level={4}>
         <span className="mr-2">Details</span>
-        <span className="text-muted-foreground hidden sm:inline">Age {selectedAge}</span>
+        <span className="text-muted-foreground hidden sm:inline">Summary</span>
       </Subheading>
       <DescriptionList>
         <DescriptionTerm>Chance of Retirement</DescriptionTerm>
