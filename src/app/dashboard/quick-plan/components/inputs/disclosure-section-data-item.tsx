3971bf9feac4daa5d73f469e2658170f56397c8b
schelskedevco <joe@schelske.dev>
2025-10-21 22:15:35 -0500

    text size

--- src/app/dashboard/quick-plan/components/inputs/disclosure-section-data-item.tsx
+++ src/app/dashboard/quick-plan/components/inputs/disclosure-section-data-item.tsx
@@ -51,10 +51,8 @@ export default function DisclosureSectionDataItem({
       </div>
       <div className="bg-emphasized-background border-border/50 flex flex-1 items-center justify-between truncate border-t border-r border-b">
         <div className="flex-1 truncate px-4 py-2">
-          <h4 className="text-sm font-medium text-stone-900 hover:text-stone-600 sm:text-base dark:text-white dark:hover:text-stone-200">
-            {name}
-          </h4>
-          <div className="text-muted-foreground text-xs sm:text-sm">{desc}</div>
+          <h4 className="text-base font-medium text-stone-900 hover:text-stone-600 dark:text-white dark:hover:text-stone-200">{name}</h4>
+          <div className="text-muted-foreground text-sm">{desc}</div>
         </div>
         <div className="shrink-0 pr-2">
           <Dropdown>
