74b28b5b3dbcd9c7ebc5311ccdf39aba8c042a83
schelskedevco <joe@schelske.dev>
2025-10-25 00:08:34 -0500

    google images in next config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'tailwindcss.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
 };
