d992515a5839a76e87fef720725f21d93278216c
schelskedevco <joe@schelske.dev>
2025-10-26 01:44:24 -0500

    to l

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -8,7 +8,7 @@ import SectionContainer from '@/components/ui/section-container';
 export default function SettingsPage() {
   return (
     <MainArea hasSecondaryColumn={false}>
-      <div className="border-border/50 from-emphasized-background to-background fixed top-0 z-10 -mx-2 hidden h-[4.3125rem] w-full border-b border-dashed bg-gradient-to-r py-4 sm:-mx-3 lg:-mx-4 lg:block">
+      <div className="border-border/50 from-emphasized-background to-background fixed top-0 z-10 -mx-2 hidden h-[4.3125rem] w-full border-b border-dashed bg-gradient-to-l py-4 sm:-mx-3 lg:-mx-4 lg:block">
         <div className="mx-4 flex items-center gap-2 sm:mx-6 lg:mx-8">
           <SettingsIcon className="text-primary h-8 w-8" aria-hidden="true" />
           <h2 className="flex items-center gap-2 text-2xl font-extrabold tracking-tight">Settings</h2>
