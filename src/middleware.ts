b9e3191b09c567a045c7a6050991bc4534ba44de
schelskedevco <joe@schelske.dev>
2025-10-29 01:37:43 -0500

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,9 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from '@/lib/auth-server';
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const token = await getToken();
   if (token) return NextResponse.redirect(new URL('/dashboard/quick-plan', request.url));
-
   return NextResponse.next();
 }
 
