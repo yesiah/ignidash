fad56c7f160cc8b81ce58d81910256bd868f3f93
schelskedevco <joe@schelske.dev>
2025-11-11 01:35:52 -0600

    update layout

--- src/app/dashboard/components/plans-list.tsx
+++ src/app/dashboard/components/plans-list.tsx
@@ -3,6 +3,8 @@
 import { Preloaded, usePreloadedQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 
+import Card from '@/components/ui/card';
+
 interface PlansListProps {
   preloadedPlans: Preloaded<typeof api.plans.listPlans>;
 }
@@ -11,10 +13,14 @@ export default function PlansList({ preloadedPlans }: PlansListProps) {
   const plans = usePreloadedQuery(preloadedPlans);
 
   return (
-    <div className="mx-auto max-w-7xl">
-      {plans.map((plan) => (
-        <div key={plan._id}>{plan.name}</div>
-      ))}
+    <div className="mx-auto w-full max-w-3xl lg:max-w-5xl xl:max-w-7xl">
+      <div className="flex w-full flex-col gap-2">
+        {plans.map((plan) => (
+          <Card key={plan._id} className="w-full">
+            Plan Name: {plan.name}
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
