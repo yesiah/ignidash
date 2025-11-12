34b77bf6289039ae343c168ecdd77e08a062e282
schelskedevco <joe@schelske.dev>
2025-11-12 15:09:41 -0600

    update

--- src/app/dashboard/components/dashboard-content.tsx
+++ src/app/dashboard/components/dashboard-content.tsx
@@ -3,7 +3,6 @@ import { api } from '@/convex/_generated/api';
 
 import { getToken } from '@/lib/auth-server';
 
-import PlansList from './plans-list';
 import PlansListV2 from './plans-list-v2';
 
 export default async function DashboardContent() {
@@ -11,6 +10,5 @@ export default async function DashboardContent() {
   if (!token) throw new Error('User not authenticated');
 
   const preloadedPlans = await preloadQuery(api.plans.listPlans, {}, { token });
-  if (false) return <PlansList preloadedPlans={preloadedPlans} />;
   return <PlansListV2 preloadedPlans={preloadedPlans} />;
 }
