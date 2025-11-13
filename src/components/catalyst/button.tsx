01cdcd7ebe87885c1893b9d0ccbb5e1d3e2db4a6
schelskedevco <joe@schelske.dev>
2025-11-13 01:19:38 -0600

    focus outline

--- src/components/catalyst/button.tsx
+++ src/components/catalyst/button.tsx
@@ -10,7 +10,7 @@ const styles = {
     // Sizing
     'px-[calc(--spacing(3.5)-1px)] py-[calc(--spacing(2.5)-1px)] sm:px-[calc(--spacing(3)-1px)] sm:py-[calc(--spacing(1.5)-1px)] sm:text-sm/6',
     // Focus
-    'focus:not-data-focus:outline-hidden data-focus:outline-2 data-focus:outline-offset-2 data-focus:outline-rose-500',
+    'focus:not-data-focus:outline-hidden data-focus:outline-2 data-focus:outline-offset-2 data-focus:outline-primary',
     // Disabled
     'data-disabled:opacity-50 data-disabled:cursor-not-allowed',
     // Icon
