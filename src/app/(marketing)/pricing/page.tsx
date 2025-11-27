241750400d6272aa0fec5fd3b86a878ead7856e1
schelskedevco <joe@schelske.dev>
2025-11-26 20:20:59 -0600

    coming soon in pricing page

--- src/app/(marketing)/pricing/page.tsx
+++ src/app/(marketing)/pricing/page.tsx
@@ -101,18 +101,18 @@ export default function PricingPage() {
                   </li>
                 ))}
               </ul>
-              <a
-                href={tier.href}
+              <button
+                disabled
                 aria-describedby={tier.id}
                 className={cn(
                   tier.featured
                     ? 'bg-rose-500 text-white shadow-xs hover:bg-rose-400 focus-visible:outline-rose-500 dark:shadow-none'
                     : 'text-rose-600 inset-ring inset-ring-rose-200 hover:inset-ring-rose-300 focus-visible:outline-rose-600 dark:bg-white/10 dark:text-white dark:inset-ring-white/5 dark:hover:bg-white/20 dark:hover:inset-ring-white/5 dark:focus-visible:outline-white/75',
-                  'mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10'
+                  'mt-8 block w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed sm:mt-10'
                 )}
               >
-                Buy {tier.name}
-              </a>
+                Coming Soon
+              </button>
             </div>
           ))}
         </div>
