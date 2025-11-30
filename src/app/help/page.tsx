681059174ab2fd4b19a791c2e5e2a72b0a292012
schelskedevco <joe@schelske.dev>
2025-11-30 16:58:07 -0600

    november 30, 2025

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -100,7 +100,7 @@ export default function HelpPage() {
             <CircleQuestionMarkIcon className="text-primary h-12 w-12" />
             <div>
               <h1 className="mb-2 text-3xl font-bold">Help Center</h1>
-              <p className="text-muted-foreground text-sm">Last updated: November 29, 2025</p>
+              <p className="text-muted-foreground text-sm">Last updated: November 30, 2025</p>
             </div>
           </div>
         </div>
