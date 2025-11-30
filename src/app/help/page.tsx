dfabee6f3eff2c27c5a033553c434dc61abbd2fd
schelskedevco <joe@schelske.dev>
2025-11-29 21:20:11 -0600

    Faq title

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -2,6 +2,7 @@ import { CircleQuestionMarkIcon } from 'lucide-react';
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
 import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline';
 
+import { Text, TextLink } from '@/components/catalyst/text';
 import { Divider } from '@/components/catalyst/divider';
 
 const faqs = [
@@ -39,7 +40,7 @@ const faqs = [
 export default function HelpPage() {
   return (
     <main className="mx-auto min-h-dvh max-w-7xl px-2 pt-[4.25rem] pb-[2.125rem] sm:px-3 lg:px-4">
-      <div className="mx-auto max-w-4xl">
+      <div className="mx-auto max-w-4xl space-y-8">
         <div className="my-8">
           <div className="flex items-center gap-4">
             <CircleQuestionMarkIcon className="text-primary h-12 w-12" />
@@ -49,13 +50,20 @@ export default function HelpPage() {
             </div>
           </div>
         </div>
-        <Divider className="my-8" />
+        <Text>
+          For any questions not covered by Help Center, please email me directly at{' '}
+          <TextLink href="mailto:joe@schelske.dev">joe@schelske.dev</TextLink> or join the{' '}
+          <TextLink href="https://discord.gg/AVNg9JCNUr">Discord server</TextLink>—I&apos;ll be there, and hopefully by the time you&apos;re
+          reading this, other users will be too!
+        </Text>
+        <Divider soft />
+        <h2 className="text-xl font-semibold tracking-tight text-gray-900 sm:text-2xl dark:text-white">Frequently asked questions</h2>
         <dl className="divide-y divide-zinc-900/10 dark:divide-white/10">
           {faqs.map((faq) => (
             <Disclosure key={faq.question} as="div" className="py-6 first:pt-0 last:pb-0">
               <dt>
                 <DisclosureButton className="group flex w-full items-start justify-between text-left text-zinc-900 dark:text-white">
-                  <span className="text-base/7 font-semibold">{faq.question}</span>
+                  <span className="text-sm/7 font-semibold">{faq.question}</span>
                   <span className="ml-6 flex h-7 items-center">
                     <PlusSmallIcon aria-hidden="true" className="size-6 group-data-open:hidden" />
                     <MinusSmallIcon aria-hidden="true" className="size-6 group-not-data-open:hidden" />
@@ -63,7 +71,7 @@ export default function HelpPage() {
                 </DisclosureButton>
               </dt>
               <DisclosurePanel as="dd" className="mt-2 pr-12">
-                <p className="text-base/7 text-zinc-600 dark:text-zinc-400">{faq.answer}</p>
+                <p className="text-sm/7 text-zinc-600 dark:text-zinc-400">{faq.answer}</p>
               </DisclosurePanel>
             </Disclosure>
           ))}
