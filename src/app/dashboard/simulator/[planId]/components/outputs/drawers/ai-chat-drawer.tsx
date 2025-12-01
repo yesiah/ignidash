44dc3b53fe7496473a19800b42f77dcf027d1df5
schelskedevco <joe@schelske.dev>
2025-12-01 14:47:28 -0600

    set up ai chat sidebar layout

--- src/app/dashboard/simulator/[planId]/components/outputs/drawers/ai-chat-drawer.tsx
+++ src/app/dashboard/simulator/[planId]/components/outputs/drawers/ai-chat-drawer.tsx
@@ -3,5 +3,9 @@ interface AIChatDrawerProps {
 }
 
 export default function AIChatDrawer({ setOpen }: AIChatDrawerProps) {
-  return <div>AI Chat Drawer</div>;
+  return (
+    <div className="-mx-2 hidden sm:-mx-3 md:fixed md:inset-y-[4.8125rem] md:flex md:w-64 md:flex-col">
+      <div className="border-border/50 flex grow flex-col border-r bg-zinc-50 dark:bg-black/10">Sidebar</div>
+    </div>
+  );
 }
