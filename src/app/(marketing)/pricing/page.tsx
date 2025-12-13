c9b67285a92409ae5641253f4d4553670f7da3ca
Joe Schelske <joe@schelske.dev>
2025-12-13 13:08:42 -0600

    update header in pricing page

--- src/app/(marketing)/pricing/page.tsx
+++ src/app/(marketing)/pricing/page.tsx
@@ -55,8 +55,7 @@ export default function PricingPage() {
         </p>
       </div>
       <p className="mx-auto mt-6 max-w-2xl text-center text-lg font-medium text-pretty text-zinc-600 sm:text-xl/8 dark:text-zinc-400">
-        The tools and insights to confidently plan your financial future. <br className="hidden sm:block" /> Get started today with no
-        credit card required.
+        The tools to confidently plan your retirement. <br className="hidden sm:block" /> No credit card required.
       </p>
       <div className="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
         {tiers.map((tier, tierIdx) => (
