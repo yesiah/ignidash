129c4cc604c0e4847a1b35487dc2ab0a23de33dc
Joe Schelske <joe@schelske.dev>
2025-12-15 23:21:57 -0600

    dashboard link

--- src/app/(marketing)/components/dashboard-link.tsx
+++ src/app/(marketing)/components/dashboard-link.tsx
@@ -4,17 +4,26 @@ import { useState } from 'react';
 import Link from 'next/link';
 import { useConvexAuth } from 'convex/react';
 
+import { cn } from '@/lib/utils';
+
 export default function DashboardLink() {
   const { isAuthenticated } = useConvexAuth();
   const [isLoading, setIsLoading] = useState(false);
 
+  const className =
+    'rounded-md bg-rose-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-rose-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600 dark:bg-rose-500 dark:hover:bg-rose-400 dark:focus-visible:outline-rose-500';
+
+  if (isLoading) {
+    return (
+      <button disabled className={cn(className, 'cursor-not-allowed opacity-50')}>
+        Loading...
+      </button>
+    );
+  }
+
   return (
-    <Link
-      onClick={() => setIsLoading(true)}
-      href="/dashboard"
-      className="rounded-md bg-rose-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-rose-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600 dark:bg-rose-500 dark:hover:bg-rose-400 dark:focus-visible:outline-rose-500"
-    >
-      {isLoading ? 'Loading...' : isAuthenticated ? 'View dashboard' : 'Start your plan'}
+    <Link onClick={() => setIsLoading(true)} href="/dashboard" className={className}>
+      {isAuthenticated ? 'View dashboard' : 'Start your plan'}
     </Link>
   );
 }
