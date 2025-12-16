542f0f59a7434ccc43dedf3db56f8eb357225c42
Joe Schelske <joe@schelske.dev>
2025-12-16 14:44:37 -0600

    select a plan

--- src/app/dashboard/insights/components/ai-output.tsx
+++ src/app/dashboard/insights/components/ai-output.tsx
@@ -11,17 +11,23 @@ export default function AIOutput() {
   return (
     <div className="-mx-2 h-full sm:-mx-3 lg:-mx-4 lg:pr-96">
       <header className="from-emphasized-background to-background border-border/50 flex items-center justify-between border-b bg-gradient-to-l px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
-        <Heading level={3}>
-          Insights for{' '}
-          <Link
-            href={`/dashboard/simulator/${selectedPlan?.id}`}
-            className="text-primary hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {selectedPlan?.name}
-          </Link>
-        </Heading>
+        {selectedPlan ? (
+          <Heading level={3}>
+            Insights for{' '}
+            <Link
+              href={`/dashboard/simulator/${selectedPlan?.id}`}
+              className="text-primary hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {selectedPlan?.name}
+            </Link>
+          </Heading>
+        ) : (
+          <Heading level={3}>
+            Select a plan <span aria-hidden="true">&rarr;</span>
+          </Heading>
+        )}
       </header>
       <div className="flex h-full flex-col items-center justify-center">AI Output</div>
     </div>
