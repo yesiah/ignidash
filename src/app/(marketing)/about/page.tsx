2f997b5203455ad51bf44205a1453e4c90a5adcc
Joe Schelske <joe@schelske.dev>
2025-12-30 02:58:44 -0600

    .

--- src/app/(marketing)/about/page.tsx
+++ src/app/(marketing)/about/page.tsx
@@ -27,7 +27,7 @@ export default function AboutPage() {
         <div className="mx-auto max-w-2xl pt-24 text-center sm:pt-40">
           <h1 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl dark:text-white">
             The <span className="bg-gradient-to-r from-pink-500 to-rose-600 bg-clip-text text-transparent">Future</span> of{' '}
-            <span className="bg-gradient-to-r from-rose-600 to-pink-500 bg-clip-text text-transparent">FIRE.</span>
+            <span className="bg-gradient-to-r from-rose-600 to-pink-500 bg-clip-text text-transparent">FIRE</span>.
           </h1>
           <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">
             An introduction to Ignidash and how it came to be.
