c0c71e82395466d7e859a41b0b227e003faa5ae3
Joe Schelske <joe@schelske.dev>
2026-01-05 18:23:29 -0600

    Revert "simulator page"
    This reverts commit 4805cd7945d4d94b78ac52af27fd67c803dfb621.

--- src/app/dashboard/simulator/[planId]/components/desktop-main-area.tsx
+++ src/app/dashboard/simulator/[planId]/components/desktop-main-area.tsx
@@ -1,25 +1,13 @@
-import SecondaryColumn from '@/components/layout/secondary-column';
-
-import NumbersColumnSections from './inputs/numbers-column-sections';
-import NumbersColumnHeader from './inputs/numbers-column-header';
 import ResultsSections from './outputs/results-sections';
 import ResultsColumnHeader from './outputs/results-column-header';
 
 export default function DesktopMainArea() {
   return (
-    <>
-      <div className="hidden xl:block">
-        <ResultsColumnHeader />
-        <div className="flex h-full flex-col pt-[4.3125rem]">
-          <ResultsSections />
-        </div>
+    <div className="hidden xl:block">
+      <ResultsColumnHeader />
+      <div className="flex h-full flex-col pt-[4.3125rem]">
+        <ResultsSections />
       </div>
-      <SecondaryColumn>
-        <NumbersColumnHeader />
-        <div className="flex h-full flex-col pt-[4.3125rem]">
-          <NumbersColumnSections />
-        </div>
-      </SecondaryColumn>
-    </>
+    </div>
   );
 }
--- src/app/dashboard/simulator/[planId]/page.tsx
+++ src/app/dashboard/simulator/[planId]/page.tsx
@@ -1,13 +1,24 @@
 import MainArea from '@/components/layout/main-area';
+import SecondaryColumn from '@/components/layout/secondary-column';
 
+import NumbersColumnSections from './components/inputs/numbers-column-sections';
+import NumbersColumnHeader from './components/inputs/numbers-column-header';
 import DesktopMainArea from './components/desktop-main-area';
 import MobileMainArea from './components/mobile-main-area';
 
 export default function SimulatorPage() {
   return (
-    <MainArea>
-      <MobileMainArea />
-      <DesktopMainArea />
-    </MainArea>
+    <>
+      <MainArea>
+        <MobileMainArea />
+        <DesktopMainArea />
+      </MainArea>
+      <SecondaryColumn>
+        <NumbersColumnHeader />
+        <div className="flex h-full flex-col pt-[4.3125rem]">
+          <NumbersColumnSections />
+        </div>
+      </SecondaryColumn>
+    </>
   );
 }
