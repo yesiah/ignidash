1748eb850a2ea99364b0d2d961476084a2e93028
Joe Schelske <joe@schelske.dev>
2026-02-01 14:07:28 -0600

    allow greater simulation seeds

--- src/lib/schemas/simulation-settings-schema.ts
+++ src/lib/schemas/simulation-settings-schema.ts
@@ -5,7 +5,7 @@ import { coerceNumber } from '@/lib/utils/zod-schema-utils';
 
 export const simulationSettingsSchema = z
   .object({
-    simulationSeed: coerceNumber(z.number().positive('Seed must be greater than 0').max(9999, 'Seed must be less than or equal to 9999')),
+    simulationSeed: coerceNumber(z.number().positive('Seed must be greater than 0').max(99999, 'Seed must be less than or equal to 99999')),
     simulationMode: z.enum(simulationModes),
     historicalStartYearOverride: coerceNumber(
       z.number().min(1928, 'Year cannot be before 1928').max(2024, 'Year cannot be after 2024')
