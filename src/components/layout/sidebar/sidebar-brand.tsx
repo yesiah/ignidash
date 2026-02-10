d48fa7edd2c5aafefe1f4f28feadc1f70e3e7043
Joe Schelske <joe@schelske.dev>
2026-02-10 15:43:06 -0600

    update version

--- src/components/layout/sidebar/sidebar-brand.tsx
+++ src/components/layout/sidebar/sidebar-brand.tsx
@@ -21,7 +21,7 @@ export default function SidebarBrand({ onClose }: SidebarBrandProps) {
           <span className="relative ml-1 text-2xl font-bold tracking-tight">
             Ignidash
             <span className="text-muted-foreground absolute top-6 left-0 text-xs/6 font-medium tracking-tighter lowercase">
-              ▸ beta v0.1.6
+              ▸ beta v0.1.7
             </span>
           </span>
         </Link>
